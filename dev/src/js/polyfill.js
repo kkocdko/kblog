4bff5e451c6acc97605163357a313521c9b91abb
kkocdko <kkocdko@163.com>
2019-09-21 10:20:16 +0800

    Change polfill dialog style and babel version

--- dev/src/js/polyfill.js
+++ dev/src/js/polyfill.js
@@ -18,43 +18,40 @@ try {
   mainScriptEl.type = 'text/babel'
   mainScriptEl.dataset.presets = 'es2017'
   const babelScriptEl = document.createElement('script')
-  babelScriptEl.src = '//cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js'
+  babelScriptEl.src = '//cdn.jsdelivr.net/npm/@babel/standalone@7.3.4/babel.min.js'
   // babelScriptEl.src = '/src/js/babel.min.js'
   babelScriptEl.onload = () => window.Babel.transformScriptTags()
   document.body.appendChild(babelScriptEl)
 
   // Show message
-
   // Source
   // document.body.insertAdjacentHTML('beforeend', `
-  // <div style="
-  //   position: fixed;
-  //   top: 50%;
-  //   left: 50%;
-  //   z-index: 9;
-  //   padding: 15px;
-  //   width: 90%;
-  //   max-width: 500px;
-  //   border-radius: 7px;
-  //   background: #fff;
-  //   box-shadow: 0 0 0 100vmax rgba(0, 0, 0, .3), 0 2px 9px 0 rgba(0, 0, 0, .3);
-  //   line-height: 1.3em;
-  //   transform: translate(-50%, -50%);
-  // ">
-  //   <p>The current browser is too old, some features on the page may not work properly</p>
-  //   <br>
-  //   <p>Please upgrade to the latest version of the browser as soon as possible.</p>
-  //   <br>
-  //   <button onclick="this.parentNode.remove()" style="
-  //     float: right;
-  //     padding: 5px 30px;
-  //     border: 1px solid #ccc;
-  //     border-radius: 4px;
+  //   <div style="
+  //     position: fixed;
+  //     top: 50%;
+  //     left: 50%;
+  //     z-index: 9;
+  //     padding: 20px;
+  //     width: 90%;
+  //     max-width: 500px;
+  //     border-radius: 7px;
   //     background: #fff;
-  //   ">OK</button>
-  // </div>
+  //     box-shadow: 0 0 0 50vmax rgba(0, 0, 0, 0.3), 0 2px 9px 0 rgba(0, 0, 0, 0.3);
+  //     line-height: 1.7em;
+  //     transform: translate(-50%, -50%);
+  //   ">
+  //     <p>Your browser is too old, page may not work properly.</p>
+  //     <p>Please upgrade to the latest browser as soon as possible.</p>
+  //     <br>
+  //     <button onclick="this.parentNode.remove()" style="
+  //       float: right;
+  //       padding: 7px 20px;
+  //       border: 1px solid #ccc;
+  //       border-radius: 4px;
+  //       background: #fff;
+  //     ">I got it</button>
+  //   </div>
   // `)
-
   // Compact
-  document.body.insertAdjacentHTML('beforeend', '<div style="position: fixed;top: 50%;left: 50%;z-index: 9;padding: 15px;width: 90%;max-width: 500px;border-radius: 7px;background: #fff;box-shadow: 0 0 0 100vmax rgba(0, 0, 0, .3), 0 2px 9px 0 rgba(0, 0, 0, .3);line-height: 1.3em;transform: translate(-50%, -50%);"><p>The current browser is too old, some features on the page may not work properly</p><br><p>Please upgrade to the latest version of the browser as soon as possible.</p><br><button onclick="this.parentNode.remove()" style="float: right;padding: 5px 30px;border: 1px solid #ccc;border-radius: 4px;background: #fff;">OK</button></div>')
+  document.body.insertAdjacentHTML('beforeend', '<div style="position:fixed;top:50%;left:50%;z-index:9;padding:20px;width:90%;max-width:500px;border-radius:7px;background:#fff;box-shadow:0 0 0 50vmax rgba(0,0,0,.3),0 2px 9px 0 rgba(0,0,0,.3);line-height:1.7em;transform:translate(-50%,-50%)"><p>Your browser is too old, page may not work properly.</p><p>Please upgrade to the latest browser as soon as possible.</p><br><button onclick="this.parentNode.remove()" style="float:right;padding:7px 20px;border:1px solid #ccc;border-radius:4px;background:#fff">I got it</button></div>')
 }
