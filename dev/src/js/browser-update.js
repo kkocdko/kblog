94ed2e48aad73ccc80b6a9334320b4b421cf1407
kkocdko <kkocdko@163.com>
2019-12-02 01:56:08 +0800

    Reduce code and change browser-update.js

--- dev/src/js/browser-update.js
+++ dev/src/js/browser-update.js
@@ -3,5 +3,5 @@ try {
   eval('async()=>{}') // eslint-disable-line
 } catch (e) {
   if (window.stop) window.stop()
-  document.body.innerHTML = '<iframe style="position:fixed;width:100%;height:100%;border:0;background:#fff" srcdoc="Loading ..." src="//browser-update.org/update.html" onload="removeAttribute(\'srcdoc\')"></iframe>'
+  document.body.innerHTML = '<iframe style="position:fixed;width:100%;height:100%;border:0;background:#fff" srcdoc="<h1 align=center><br><br>Loading ...</h1>" src="//browser-update.org/update.html" onload="removeAttribute(\'srcdoc\')"></iframe>'
 }
