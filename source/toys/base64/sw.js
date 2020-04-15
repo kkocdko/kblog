0b401bceb9e7f37c8e56c8178afb26d3fe9c338b
kkocdko <kkocdko@163.com>
2020-04-15 22:19:13 +0800

    Update toys

--- source/toys/base64/sw.js
+++ source/toys/base64/sw.js
@@ -4,41 +4,41 @@ const cacheName = "base64";
 const filesList = [
   {
     url: "./",
-    updateDate: 1586545603983,
+    updateDate: 1586884042956,
   },
   {
     url:
       "//cdn.jsdelivr.net/npm/material-components-web@5.1.0/dist/material-components-web.min.css",
-    updateDate: 1586545603983,
+    updateDate: 1586884042956,
   },
   {
     url:
       "//cdn.jsdelivr.net/npm/material-components-web@5.1.0/dist/material-components-web.min.js",
-    updateDate: 1586545603983,
+    updateDate: 1586884042956,
   },
   {
-    url: "/toy/misc/material-components-web-kmod.css",
-    updateDate: 1586545603983,
+    url: "../misc/material-components-web-kmod.css",
+    updateDate: 1586884042956,
   },
   {
     url: "style.css",
-    updateDate: 1586545603983,
+    updateDate: 1586884042956,
   },
   {
     url: "script.js",
-    updateDate: 1586545603983,
+    updateDate: 1586884042956,
   },
   {
     url: "favicon.svg",
-    updateDate: 1586545603983,
+    updateDate: 1586884042956,
   },
   {
     url: "favicon.png",
-    updateDate: 1586545603983,
+    updateDate: 1586884042956,
   },
   {
     url: "manifest.json",
-    updateDate: 1586545603983,
+    updateDate: 1586884042956,
   },
 ];
 
--- source/toys/qrcode/script.js
+++ source/toys/qrcode/script.js
@@ -4,15 +4,11 @@ const svgContent = document.querySelector("#svg-content");
 const svgViewer = document.querySelector("#svg-viewer");
 
 const refreshQrCode = () => {
-  svgViewer.innerHTML = new window.QrCode(svgContent.value, "L").toSvg(10);
-};
-
-const downloadSvg = () => {
-  downloadTextContent(`qrcode_${Date.now()}.svg`, svgViewer.innerHTML);
+  svgViewer.innerHTML = new QrCode(svgContent.value, "L").toSvg(10);
 };
 
 const downloadTextContent = (name, contentStr) => {
-  const blob = new window.Blob([contentStr]);
+  const blob = new Blob([contentStr]);
   const href = URL.createObjectURL(blob);
   URL.revokeObjectURL(blob);
   const aTag = document.createElement("a");
@@ -21,7 +17,14 @@ const downloadTextContent = (name, contentStr) => {
   aTag.click();
 };
 
-svgContent.value = window.location.href;
+const downloadSvg = () => {
+  downloadTextContent(`qrcode_${Date.now()}.svg`, svgViewer.innerHTML);
+};
+
+svgContent.value = location.href;
+svgContent.click();
+svgContent.focus();
+
 refreshQrCode();
 
 document.querySelector("#refresh-btn").addEventListener("click", refreshQrCode);
