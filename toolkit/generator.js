9636150ac61d339559f9887ba65e9160f8e80e33
kkocdko <kkocdko@163.com>
2020-07-20 02:49:35 +0800

    Update generator

--- toolkit/generator.js
+++ toolkit/generator.js
@@ -2,40 +2,97 @@
 
 console.time("generate time");
 process.on("exit", () => console.timeEnd("generate time"));
+// setTimeout(() => process.exit(), 900);
 
 const path = require("path");
-const fs = require("file-system"); // This is not native fs
+const fs = require("fs");
 const marked = require("marked");
 
-const mfs = {
-  r2a: require("path").join.bind(null, __dirname, ".."), // Relative to absolute
-  readFileStrSync(path) {
-    return fs.readFileSync(path).toString();
-  },
-  writeFile(path, data) {
-    fs.writeFile(path, data);
-  },
-  copyFile(sourcePath, targetPath) {
-    fs.copyFile(sourcePath, targetPath);
-  },
-  removeDirSync(path) {
+const fsex = (() => {
+  const r2a = (relativePath) =>
+    path.isAbsolute(relativePath)
+      ? relativePath
+      : path.join(__dirname, "..", relativePath);
+  const makeDir = (dirPath, madeDirPath) => {
     try {
-      // Ignore error when dir is not existent
-      fs.rmdirSync(path);
-    } catch {}
-  },
-  copyDir(sourcePath, targetPath, filter = ["**/*"]) {
-    fs.recurse(sourcePath, filter, (absolute, relative) => {
-      fs.copyFile(absolute, path.join(targetPath, relative));
+      fs.mkdirSync(dirPath);
+      madeDirPath = madeDirPath || dirPath;
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        madeDirPath = makeDir(path.dirname(dirPath), madeDirPath);
+        makeDir(dirPath, madeDirPath);
+      } else if (!fs.statSync(dirPath).isDirectory()) {
+        throw error;
+      }
+    }
+    return madeDirPath;
+  };
+  const removeDir = (dirPath) => {
+    if (fs.existsSync(dirPath)) {
+      fs.readdirSync(dirPath).forEach((item) => {
+        const itemPath = path.join(dirPath, item);
+        if (fs.statSync(itemPath).isDirectory()) {
+          removeDir(itemPath);
+        } else {
+          fs.unlinkSync(itemPath);
+        }
+      });
+      fs.rmdirSync(dirPath);
+    }
+  };
+  const readFile = (filePath) => fs.readFileSync(filePath).toString();
+  const readDir = (dirPath) =>
+    fs.readdirSync(dirPath).map((item) => path.join(dirPath, item));
+  const writeFile = (filePath, data) => {
+    makeDir(path.parse(filePath).dir);
+    fs.writeFileSync(filePath, data);
+  };
+  const copyFile = (sourceFilePath, targetFilePath) => {
+    makeDir(path.parse(targetFilePath).dir);
+    fs.copyFileSync(sourceFilePath, targetFilePath);
+  };
+  const copyDir = (sourceDirPath, targetDirPath) => {
+    if (!fs.existsSync(targetDirPath)) {
+      makeDir(targetDirPath);
+    }
+    fs.readdirSync(sourceDirPath).forEach((item) => {
+      const sourceItemPath = path.join(sourceDirPath, item);
+      const targetItemPath = path.join(targetDirPath, item);
+      if (fs.statSync(sourceItemPath).isDirectory()) {
+        copyDir(sourceItemPath, targetItemPath);
+      } else {
+        fs.copyFileSync(sourceItemPath, targetItemPath);
+      }
     });
-  },
-  walkDirSync(dirPath, filter = ["**/*"], callback) {
-    fs.recurseSync(dirPath, filter, callback);
-  },
-};
+  };
+  return {
+    makeDir(dirPath) {
+      makeDir(r2a(dirPath));
+    },
+    removeDir(dirPath) {
+      removeDir(r2a(dirPath));
+    },
+    readFile(filePath) {
+      return readFile(r2a(filePath));
+    },
+    readDir(dirPath) {
+      return readDir(r2a(dirPath));
+    },
+    writeFile(filePath, data) {
+      writeFile(r2a(filePath), data);
+    },
+    copyFile(sourceFilePath, targetFilePath) {
+      copyFile(r2a(sourceFilePath), r2a(targetFilePath));
+    },
+    copyDir(sourceDirPath, targetDirPath) {
+      copyDir(r2a(sourceDirPath), r2a(targetDirPath));
+    },
+    joinPath: path.join,
+  };
+})();
 
 const minifier = (() => {
-  if (process.argv.includes("--dev-mode")) {
+  if (process.argv.includes("--debug")) {
     return {
       html: (s) => s,
       htmlMd: (s) => s,
@@ -95,7 +152,7 @@ const minifier = (() => {
 })();
 
 const parseMdFile = (filePath) => {
-  const str = mfs.readFileStrSync(filePath);
+  const str = fsex.readFile(filePath);
   const readMeta = (key) => {
     const matched = str.match(`\n${key}: ([^\n]*)`);
     return matched ? matched[1] : null;
@@ -106,15 +163,14 @@ const parseMdFile = (filePath) => {
 };
 
 const makePage = (() => {
-  const templateFileStr = mfs.readFileStrSync(mfs.r2a("/units/page.html"));
-  const template = minifier.html(templateFileStr);
+  const template = minifier.html(fsex.readFile("./units/page.html"));
   return ({
     realPath = "",
     path = "",
     title = "",
     description = "",
     content = "",
-    type = "html", // "html" | "markdown"
+    type, // "html" | "markdown"
   }) => {
     switch (type) {
       case "html":
@@ -124,75 +180,70 @@ const makePage = (() => {
         content = `<article class="card">${marked(content)}</article>`;
         content = minifier.htmlMd(content);
         break;
+      default:
+        throw "page type is not defined";
     }
     const result = template
       .replace(/{{ title }}/g, title)
       .replace(/{{ description }}/g, description)
       .replace(/{{ content }}/g, content);
     const targetPath = realPath
-      ? mfs.r2a("/public", realPath)
-      : mfs.r2a("/public", path, "index.html");
-    mfs.writeFile(targetPath, result);
+      ? fsex.joinPath("./public", realPath)
+      : fsex.joinPath("./public", path, "/index.html");
+    fsex.writeFile(targetPath, result);
   };
 })();
 
 // Initialization
 {
-  mfs.removeDirSync(mfs.r2a("/public"));
+  fsex.removeDir("./public");
 
-  mfs.copyFile(mfs.r2a("/units/_gitignore"), mfs.r2a("/public/.gitignore"));
-  mfs.copyFile(mfs.r2a("/units/_nojekyll"), mfs.r2a("/public/.nojekyll"));
-  mfs.copyFile(mfs.r2a("/units/favicon.ico"), mfs.r2a("/public/favicon.ico"));
-  mfs.copyFile(mfs.r2a("/units/favicon.svg"), mfs.r2a("/public/favicon.svg"));
+  fsex.copyFile("./units/_gitignore", "./public/.gitignore");
+  fsex.copyFile("./units/_nojekyll", "./public/.nojekyll");
+  fsex.copyFile("./units/favicon.ico", "./public/favicon.ico");
+  fsex.copyFile("./units/favicon.svg", "./public/favicon.svg");
 
   // bundle.css
   {
-    const mdCss = mfs.readFileStrSync(mfs.r2a("/units/markdown.css"));
-    const appCss = mfs.readFileStrSync(mfs.r2a("/units/app.css"));
+    const mdCss = fsex.readFile("./units/markdown.css");
+    const appCss = fsex.readFile("./units/app.css");
     const cssStr = mdCss + appCss;
     const result = minifier.css(cssStr);
-    mfs.writeFile(mfs.r2a("/public/bundle.css"), result);
+    fsex.writeFile("./public/bundle.css", result);
   }
 
   // bundle.js
   {
-    const extraJs = mfs.readFileStrSync(mfs.r2a("/units/extra.js"));
-    const extraHtmlFileStr = mfs.readFileStrSync(mfs.r2a("/units/extra.html"));
+    const extraJs = fsex.readFile("./units/extra.js");
+    const extraHtmlFileStr = fsex.readFile("./units/extra.html");
     const extraHtml = minifier.html(extraHtmlFileStr);
-    const appJs = mfs.readFileStrSync(mfs.r2a("/units/app.js"));
+    const appJs = fsex.readFile("./units/app.js");
     const jsStr = extraJs.replace("/* @ extra.html */", extraHtml) + appJs;
     const result = minifier.js(`(()=>{${jsStr}})()`);
-    mfs.writeFile(mfs.r2a("/public/bundle.js"), result);
+    fsex.writeFile("./public/bundle.js", result);
   }
 
   // check.js
   {
-    const checkJs = mfs.readFileStrSync(mfs.r2a("/units/check.js"));
+    const checkJs = fsex.readFile("./units/check.js");
     const result = minifier.js(checkJs);
-    mfs.writeFile(mfs.r2a("/public/check.js"), result);
+    fsex.writeFile("./public/check.js", result);
   }
 }
 
 // Misc
 {
-  mfs.copyDir(mfs.r2a("/source/toys"), mfs.r2a("/public/toy"), [
-    "**/*",
-    "!.git/**/*",
-  ]);
-
-  mfs.copyDir(mfs.r2a("/source/res"), mfs.r2a("/public/res"), [
-    "**/*",
-    "!.git/**/*",
-  ]);
+  fsex.copyDir("./source/toys", "./public/toy");
+  fsex.copyDir("./source/res", "./public/res");
 }
 
 // Pages
 const pagesList = [];
 {
-  mfs.walkDirSync(mfs.r2a("/source/pages"), ["*.md"], (absolute) => {
-    const { readMeta, content } = parseMdFile(absolute);
+  fsex.readDir("./source/pages").forEach((filePath) => {
+    const { readMeta, content } = parseMdFile(filePath);
     const attr = {
-      name: path.parse(absolute).name,
+      name: path.parse(filePath).name,
       title: readMeta("title"),
       description: readMeta("description"),
     };
@@ -209,8 +260,8 @@ const pagesList = [];
 // Posts
 const postsList = [];
 {
-  mfs.walkDirSync(mfs.r2a("/source/posts"), ["*.md"], (absolute) => {
-    const { readMeta, content } = parseMdFile(absolute);
+  fsex.readDir("./source/posts").forEach((filePath) => {
+    const { readMeta, content } = parseMdFile(filePath);
     const attr = {
       id: readMeta("date").replace(/:|-|\s/g, ""),
       date: readMeta("date").split(" ")[0],
@@ -226,8 +277,11 @@ const postsList = [];
       content,
     });
     postsList.push(attr);
-    if (attr.date + " " + attr.title !== path.parse(absolute).name) {
-      console.warn(`file name is not standard: ${attr.title}`);
+    if (
+      `${attr.id.slice(0, 8)}-${attr.id.slice(8, 12)} ${attr.title}` !==
+      path.parse(filePath).name
+    ) {
+      console.warn(`filename is not standard: ${attr.title}`);
     }
   });
   postsList.sort((post1, post2) => post2.id - post1.id);
@@ -276,6 +330,7 @@ const postsList = [];
     if (curPage === 1) {
       makePage({
         path: "/",
+        type: "html",
         title: "Homepage",
         description: "Welcome to my blog!",
         content: htmlStr,
@@ -283,6 +338,7 @@ const postsList = [];
     } else {
       makePage({
         path: `/home/${curPage}/`,
+        type: "html",
         title: `Home: ${curPage}`,
         content: htmlStr,
       });
@@ -309,6 +365,7 @@ const postsList = [];
   htmlStr += "</div>";
   makePage({
     path: "/archive/",
+    type: "html",
     title: "Archive",
     content: htmlStr,
   });
@@ -327,6 +384,7 @@ const postsList = [];
     htmlStr += "</div>";
     makePage({
       path: `/archive/${year}`,
+      type: "html",
       title: `${year} - Archive`,
       content: htmlStr,
     });
@@ -351,6 +409,7 @@ const postsList = [];
   htmlStr += "</div>";
   makePage({
     path: "/category/",
+    type: "html",
     title: "Category",
     content: htmlStr,
   });
@@ -369,6 +428,7 @@ const postsList = [];
     htmlStr += "</div>";
     makePage({
       path: `/category/${category}`,
+      type: "html",
       title: `${category} - Category`,
       content: htmlStr,
     });
@@ -395,6 +455,7 @@ const postsList = [];
   htmlStr += "</div>";
   makePage({
     path: "/tag/",
+    type: "html",
     title: "Tag",
     content: htmlStr,
   });
@@ -413,6 +474,7 @@ const postsList = [];
     htmlStr += "</div>";
     makePage({
       path: `/tag/${tag}`,
+      type: "html",
       title: `${tag} - Tag`,
       content: htmlStr,
     });
@@ -425,7 +487,9 @@ const postsList = [];
     realPath: "/404.html",
     title: "404 not found",
     type: "markdown",
-    content: '<h1 style="text-align:center;border:none">404 not found</h1>',
+    content:
+      '<h1 style="text-align:center;border:none">404 not found</h1>' +
+      '<script>if(location.pathname.match(/post\\/\\d{14}/)){alert("旧的14位博文ID已经弃用，将跳转到新地址。\\nOld 14-dight post id is deprecate, will jump to new address.");location.pathname=location.pathname.slice(0,18)}</script>',
   });
 }
 
@@ -433,13 +497,13 @@ const postsList = [];
 {
   const domain = "https://kkocdko.github.io";
 
-  mfs.writeFile(
-    mfs.r2a("/public/robots.txt"),
+  fsex.writeFile(
+    "./public/robots.txt",
     `User-agent: *\nAllow: /\nSitemap: ${domain}/sitemap.xml`
   );
 
-  mfs.writeFile(
-    mfs.r2a("/public/sitemap.xml"),
+  fsex.writeFile(
+    "./public/sitemap.xml",
     '<?xml version="1.0" encoding="UTF-8"?>' +
       '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
       pagesList
@@ -451,8 +515,8 @@ const postsList = [];
       "</urlset>"
   );
 
-  mfs.writeFile(
-    mfs.r2a("/public/feed.xml"),
+  fsex.writeFile(
+    "./public/feed.xml",
     '<?xml version="1.0" encoding="UTF-8"?>' +
       '<rss version="2.0">' +
       "<channel>" +
