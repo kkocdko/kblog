2b46a1ca16aa6171dd85defe89aeb5bec295e59c
kkocdko <kkocdko@163.com>
2020-11-20 12:42:05 +0800

    Improve user experience

--- units/app.js
+++ units/app.js
@@ -35,7 +35,7 @@ onpopstate = (noToTop) => {
       );
       listenSpaLinks();
       document.body.className = "loaded"; // Also replace the "loading"
-      setTimeout(() => (document.body.className = ""), 500);
+      setTimeout(() => (document.body.className = ""), 300);
     });
 };
 
