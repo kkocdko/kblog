7c40f80e8117be0aea4f385f8fe6fd4c1106792e
kkocdko <kkocdko@163.com>
2021-02-18 02:03:57 +0800

    Speed up scripts about develop

--- toolkit/serve.js
+++ toolkit/serve.js
@@ -1,10 +1,6 @@
 "use strict";
 
-const cfg = {
-  host: "127.0.0.1:4000",
-  delay: 0,
-};
-
+const host = "127.0.0.1:4000";
 const mime = {
   html: "text/html;charset=utf-8",
   css: "text/css",
@@ -15,23 +11,18 @@ const mime = {
   jpeg: "image/jpeg",
   webp: "image/webp",
 };
-
 const fs = require("fs");
-const path = require("path");
-const r2a = path.join.bind(null, __dirname, "../public");
-const serve = require("http").createServer;
-serve(async (req, res) => {
-  if (cfg.delay) await new Promise((r) => setTimeout(r, cfg.delay));
+const r2a = require("path").join.bind(null, __dirname, "../public");
+const createServer = require("http").createServer;
+createServer(({ url }, res) => {
   const pair = [
-    [200, r2a(req.url)],
-    [200, r2a(req.url + "index.html")],
+    [200, r2a(url)],
+    [200, r2a(url + "index.html")],
     [404, r2a("404.html")],
   ].find(([_, p]) => fs.existsSync(p) && fs.statSync(p).isFile());
   if (!pair) return res.writeHead(404).end("404 Not Found");
   const [status, filePath] = pair;
-  const mimeType = mime[path.extname(filePath).slice(1)];
-  if (mimeType) res.setHeader("content-type", mimeType);
+  res.setHeader("content-type", mime[filePath.split(".").pop()] || "");
   res.writeHead(status).end(fs.readFileSync(filePath));
-}).listen(...cfg.host.split(":").reverse());
-console.info("server address: " + cfg.host);
-process.stdin.on("data", () => process.exit());
+}).listen(...host.split(":").reverse());
+console.info("server address: " + host);
